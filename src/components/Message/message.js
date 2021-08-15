88da67fa1f7e258c312d4e4779aba63083b1cc56
Андрей <83415529+AndreyDanich@users.noreply.github.com>
2021-08-16 00:08:44 +0300

    создал приложение

---
+++ src/components/Message/message.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+export function Message(props) {
+    return (
+        <h1>{props.header}</h1>
+    )
+}
+
+
+
+// ReactDom.render(
+//     element,
+//     document.getElementById('root')
+// );
+
