b6760aa81cffb6bea08a5a3c7d20cb2340e88b32
Андрей <83415529+AndreyDanich@users.noreply.github.com>
2021-08-19 23:52:47 +0300

    Урок 2
    Не смог связать событие по кнопке, попытку закомментировал в messageList.js

--- src/components/MessageList/messageList.js
+++ src/components/MessageList/messageList.js
@@ -1,28 +1,44 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 export function MessageList() {
+    // const user = { author: "", text: "" };
+    // const [messages, setMessages] = useState(0);
+
+    // const updateMessage = () => {
+    //     setMessages((prevMessages) => prevMessages + 1);
+    // }
+
+    // useEffect(() => {
+    //     console.log(messages);
+    // }, [messages]);
+
+    // return (
+    //     (messages.map((message) => <div>{message}</div>)),
+    //     (<div className="input" >
+    //         <input className="formSize" type="text" />{messages}
+    //         <button onClick={updateMessage}>push</button>
+    //     </div>)
+    // )
+
     const [messages, setMessages] = useState([
-        "message 1",
-        "message 2",
-        "message 3",
+        "messages 1",
+        "messages 2",
+        "messages 3",
     ]);
 
-    return messages.map((message) => <div>{message}</div>);
+    return (
+        (messages.map((message) => <div>{message}</div>)),
+
+        (<div className="input" >
+            <input className="formSize" type="text" />
+            <button>push</button>
+        </div>)
+    )
 }
 
-
-
-
-// import React, { useState } from 'react';
-
-// export function MessageList() {
-//     const [name, setCount] = useState([]);
-//     return (
-//         <div>
-//             <input value={name} type="text" />
-//             <button className="counter-button">Click!</button>
-//         </div>
-//     )
-// }
+// (<div className="input" >
+//             <input className="formSize" type="text" />
+//             <button>push</button>
+//         </div>)
 
